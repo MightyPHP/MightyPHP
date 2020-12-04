8cd8cecf1fa6ed66a7e1556837e2f33b9ac10746
Desmond Chin <Desmond>
2020-12-04 21:32:23 +0800

    code clean up

--- Application/Controllers/HomeController.php
+++ Application/Controllers/HomeController.php
@@ -2,16 +2,12 @@
 namespace Application\Controllers;
 
 use Application\Controllers\Controller;
-use MightyCore\RESPONSE;
-use MightyCore\VERSION;
-use MightyCore\MOMENT;
+use MightyCore\Version;
 class HomeController extends Controller
 {
     public function welcome() {  
-        RESPONSE::return($this->_view('welcome')->render(array(
-            'MightyCoreVersion' => VERSION::getVersion(),
-            "date" => MOMENT::now()->toDateTimeString('Y-m-d')
-        )));
+        return view('welcome', array(
+            'MightyCoreVersion' => Version::getVersion(),
+        ));
     }
-    
 }
