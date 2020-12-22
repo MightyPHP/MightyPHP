77f4e2b58e4f94265b732b709ac85f3c8c05fa41
Desmond Chin <Desmond>
2020-12-22 17:11:51 +0800

    added scheduler feature

---
+++ Application/Console/Scheduler.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Application\Console;
+
+use MightyCore\Command\Scheduler\SchedulerCore;
+
+class Scheduler extends SchedulerCore
+{
+    public function run(){
+        // write your scheduled tasks here
+    }
+}
