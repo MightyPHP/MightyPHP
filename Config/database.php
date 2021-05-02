66f857bed849e799868f5984ad7389c1c19cc75a
Desmond Chin <Desmond>
2021-05-02 16:37:40 +0800

    added perpage config

--- Config/database.php
+++ Config/database.php
@@ -14,5 +14,11 @@ return [
     'password' => env('DATABASE_PASSWORD', null),
     'user' => env('DATABASE_USER', null),
     'schema' => env('DATABASE_SCHEMA', null)
-  ]
+  ],
+
+  /**
+   * For the use of paginate() method
+   * Number of default records per page to return
+   */
+  'perPage' => env('DATABASE_PER_PAGE', 10)
 ];
